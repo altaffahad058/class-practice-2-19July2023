10cb18dbe66b462aa76ce27fd7a0cc371e660197
Fahad Altaf <altaffahad058@gmail.com>
2023-07-19 22:40:20 +0500

    sum print code

--- try2.cpp
+++ try2.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello world.";
+    int a = 1, b = 2, c = 0;;
+    cout<<"Hello world."<<endl;
+   
+    c = a+b;
+    cout<<c<<endl;
+   
     system("pause");
     return 0;
 }
