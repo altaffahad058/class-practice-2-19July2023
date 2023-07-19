0a588039c83294e78e904827fec64c6f8ad7ff45
Fahad Altaf <altaffahad058@gmail.com>
2023-07-19 22:55:54 +0500

    DMAS applied code

--- try2.cpp
+++ try2.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello world.";
+    int a = 1, b = 2, c = 3, d=4, e=5, f=0;
+    cout<<"Hello world."<<endl;
+   
+    f = (((a/b)*c)+d)-e;
+    cout<<f<<endl;
+   
     system("pause");
     return 0;
 }
